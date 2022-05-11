04a7c8e9d654aefdc8da1b51d8f5062bd71d47f9
DESKTOP-VU5JPAT\Natella <gomza.natella@gmail.com>
2022-05-12 00:25:35 +0300

    with formValidation

--- Ishop 3/components/productCard.js
+++ Ishop 3/components/productCard.js
@@ -15,6 +15,16 @@ class ProductCard extends React.Component {
         cbRefreshInfo: PropTypes.func,
     };
 
+    state = {
+        isFormValid: true,
+        isValidProduct: {
+            isValidName: true,
+            isValidPrice: true,
+            isValidUrl: true,
+            isValidQuantity: true
+        }
+    }
+
     refreshInfo = (event) => {
         event.preventDefault();
 
@@ -27,6 +37,54 @@ class ProductCard extends React.Component {
 
     }
 
+    isValidForm = () => {
+        if (  !this.state.isValidProduct.isValidName || !this.state.isValidProduct.isValidUrl || !this.state.isValidProduct.isValidPrice || !this.state.isValidProduct.isValidQuantity) {
+            this.setState({isFormValid: false})
+        } else {
+            this.setState({ isFormValid: true })
+        }
+    }
+
+    itemNameChanged = (event) => {
+        if (event.target.name === "name" && !event.target.value) {
+            this.state.isValidProduct.isValidName = false;
+        } else {
+            this.state.isValidProduct.isValidName = true;
+        }
+
+        this.isValidForm();
+    }
+
+    itemUrlChahged = (event) => {
+        if (event.target.name === "url" && !event.target.value) {
+            this.state.isValidProduct.isValidUrl = false;
+        } else {
+            this.state.isValidProduct.isValidUrl = true;
+        }
+
+        this.isValidForm();
+    }
+
+    itemPriceChanged = (event) => {
+        if (event.target.name === "price" && !parseInt(event.target.value) || parseInt(event.target.value) <= 0) {
+            this.state.isValidProduct.isValidPrice = false;
+        } else {
+            this.state.isValidProduct.isValidPrice = true;
+        }
+
+        this.isValidForm();
+    }
+
+    itemQuantityChanged = (event) => {
+        if (event.target.name === "quantity" && !parseInt(event.target.value) || parseInt(event.target.value) <= 0) {
+            this.state.isValidProduct.isValidQuantity = false;
+        } else {
+            this.state.isValidProduct.isValidQuantity = true;
+        }
+
+        this.isValidForm();
+    }
+
     render() {
         if (this.props.workMode === 2) {
             return (
@@ -35,21 +93,25 @@ class ProductCard extends React.Component {
                     <form method="post" id="form" onSubmit={this.refreshInfo}>
                         <div>
                             <span> Name </span>
-                            <input type="text" name="name" defaultValue={this.props.name} onChange={this.itemChanged} />
+                            <input type="text" name="name" defaultValue={this.props.name} onChange={this.itemNameChanged} />
+                            {(!this.state.isValidProduct.isValidName) && <span> Please, fill the field </span>}
                         </div>
                         <div>
                             <span> Price </span>
-                            <input type="text" name="price" defaultValue={this.props.price} />
+                            <input type="text" name="price" defaultValue={this.props.price} onChange={this.itemPriceChanged} />
+                            {(!this.state.isValidProduct.isValidPrice) && <span> Please, fill the field. Field should be a number </span>}
                         </div>
                         <div>
                             <span> URL </span>
-                            <input type="text" name="url" defaultValue={this.props.photo} />
+                            <input type="text" name="url" defaultValue={this.props.photo} onChange={this.itemUrlChahged} />
+                            {(!this.state.isValidProduct.isValidUrl) && <span> Please, fill the field </span>}
                         </div>
                         <div>
                             <span> Quantity </span>
-                            <input type="text" name="quantity" defaultValue={this.props.availableAmmount} />
+                            <input type="text" name="quantity" defaultValue={this.props.availableAmmount} onChange={this.itemQuantityChanged} />
+                            {(!this.state.isValidProduct.isValidQuantity) && <span> Please, fill the field. Field should be a number </span>}
                         </div>
-                        <input type="submit" value="Save" />
+                        <input type="submit" value="Save" disabled={this.state.isFormValid === false} />
                         <input type="button" value="Cancel" />
                     </form>
                 </div>
